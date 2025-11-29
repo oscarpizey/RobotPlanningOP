91d1f6e4259ff13fb32b9521abe4e1249713063f
oscarpizey <egyop3@nottingham.ac.uk>
2025-11-29 15:44:40 +0000

    Initial Testing

---
+++ DrawingRobotOP.c
@@ -0,0 +1,12 @@
+struct loadFont(char file);
+{
+    return 0
+}
+
+int main()
+{
+    char fontFile[] = "SingleStrokeFontData.txt";
+    struct fontData = loadFont(fontFile);
+    printf(fontFile);
+    return 0
+}
